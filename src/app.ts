020f082699996f78f3c0acb08408002afa037275
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-02 16:54:38 +0900

    add: app.ts

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Scary nodemonster!");
+});
+
+app.listen("3000", () => {
+  console.log(`
+  ################################################
+  Server listening on port: 3000
+  ################################################
+`);
+});
