723268c9b72e8af179a9806858aa0cc52836e124
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-03 18:46:47 +0900

    add: add express.ts

---
+++ src/loader/express.ts
@@ -0,0 +1,51 @@
+import fs from 'fs';
+import express, { NextFunction, Request, Response, Application } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { ServerResponse } from 'http';
+import { config } from '../config';
+import { nodemonsterRouter } from '../route';
+import { HttpError, NotFoundURLError } from '../shared/exception';
+import { logger } from '../shared/logger';
+
+export const loadExpress = (app: Application) => {
+    app.set('port', config.ServicePort || '3000');
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        morgan('combined', {
+            stream: fs.createWriteStream('./logs/log.log', { encoding: 'utf-8' }),
+        })(req, res as ServerResponse, next);
+    });
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        express.json()(req, res as ServerResponse, next);
+    });
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        express.urlencoded({ extended: false })(req, res as ServerResponse, next);
+    });
+
+    const CorsOption = {
+        origin: '*',
+        method: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
+        credentials: true,
+    };
+
+    app.use(cors(CorsOption));
+
+    app.use('/', nodemonsterRouter());
+
+    app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+        logger.info(err.message);
+        const statusCode: number = err.statusCode || 500;
+        res.status(statusCode).json({
+            statusCode,
+            message: err.message,
+            timeStamp: new Date().toTimeString(),
+        });
+    });
+
+    app.listen(app.get('port'), () => {
+        console.log('server on', app.get('port'));
+    });
+};
