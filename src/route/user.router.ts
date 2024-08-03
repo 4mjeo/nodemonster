e372a596207993e65ad90b3e7f0aa73e542b4077
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-03 18:27:05 +0900

    update: delete user import

--- src/route/user.router.ts
+++ src/route/user.router.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import { UserController } from '../controller/user.controller';
 import { errorHandler } from '../middleware/errorHandler';
 import { verifyRefreshTokenMiddleware, verifyTokenMiddleware } from '../middleware/verifyToken';
-import { User } from '../entity/user';  
 
 const router: Router = Router();
 export const userServiceRouter = (app: Router) => {
