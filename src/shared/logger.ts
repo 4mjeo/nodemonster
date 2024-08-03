a4c6f8e3715c119cb532a1328aae815862e44fd5
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-03 22:23:43 +0900

    update: delete space

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -4,7 +4,7 @@ const logger: Logger = createLogger({
   level: "info",
   format: format.json(),
   transports: [
-    new transports.File({ filename: "./logs/combined/log " }),
+    new transports.File({ filename: "./logs/combined/log" }),
     new transports.File({ filename: "./logs/error.log", level: "error" }),
   ],
 });
