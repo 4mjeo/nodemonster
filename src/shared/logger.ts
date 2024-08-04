df83a7183a88c4036b8a9abf701e09fb86e9f8b1
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-04 17:58:45 +0900

    chore: lint

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,11 +1,11 @@
-import { createLogger, Logger, format, transports } from "winston";
+import { createLogger, Logger, format, transports } from 'winston';
 
 const logger: Logger = createLogger({
-  level: "info",
+  level: 'info',
   format: format.json(),
   transports: [
-    new transports.File({ filename: "./logs/combined/log" }),
-    new transports.File({ filename: "./logs/error.log", level: "error" }),
+    new transports.File({ filename: './logs/combined/log' }),
+    new transports.File({ filename: './logs/error.log', level: 'error' }),
   ],
 });
 
