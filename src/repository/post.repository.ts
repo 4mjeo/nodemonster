889be4fa7e95633a3685bee5c3782cd81b7ff1e3
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-05 23:19:48 +0900

    add: add post.repository.ts

---
+++ src/repository/post.repository.ts
@@ -0,0 +1,73 @@
+import { EntityRepository, getCustomRepository, Repository } from 'typeorm';
+import { Post } from '../entity/post';
+import { User } from '../entity/user';
+import { PostInfo, PostUpdateInfo} from '../shared/DataTransferObject';
+
+@EntityRepository(Post)
+export class PostRepository extends Repository<Post> {
+    static getQueryRepository() {
+        return getCustomRepository(PostRepository);
+    }
+
+    async createPost(postInfo: PostInfo, user: User): Promise<Post> {
+        const newPost = new Post();
+
+        newPost.title = postInfo.title;
+        newPost.content = postInfo.content;
+        newPost.seller = user.id;
+
+        return this.save(newPost);
+    }
+
+    async updatePost(postUpdateInfo: PostUpdateInfo, user: User) {
+        return this.update(
+            {
+                id: postUpdateInfo.id,
+                seller: user.id,
+            },
+            {
+                title: postUpdateInfo.title,
+                content: postUpdateInfo.content,
+            },
+        );
+    }
+
+    async deletePost(postId: number, user: User) {
+        return this.delete({
+            id: postId,
+            seller: user.id
+        });
+    }
+
+    async getOnePost(postId: number): Promise<Post> {
+		return this.createQueryBuilder('post')
+			.select('post.id')
+			.addSelect('post.title')
+			.addSelect('post.text')
+			.addSelect('post.createdAt')
+			.addSelect('post.updatedAt')
+			.addSelect('user.nickname')
+			.addSelect('seller.nickname')
+			.where('post.id = :postId', {
+				postId,
+			})
+			.getOne();
+	}
+
+    async getAllPost() {
+		return this.createQueryBuilder('post')
+			.select('post.id')
+			.addSelect('post.title')
+			.addSelect('post.text')
+			.addSelect('post.createdAt')
+			.addSelect('post.updatedAt')
+			.addSelect('user.nickname')
+			.addSelect('seller.nickname')
+			.getMany();
+	}
+
+    async checkPost(postId: number): Promise<Post> {
+        return this.findOne({ id: postId });
+    }
+
+}
