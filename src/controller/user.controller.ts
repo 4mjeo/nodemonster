82e60dc0bf2969a04e3908b27e1e6c8076894902
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-05 14:50:08 +0900

    update: lint & add method

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,75 +1,74 @@
-import { response } from "express";
-import { REFRESH_TOKEN_COOKIE_KEY } from "../constants/auth";
+import { REFRESH_TOKEN_COOKIE_KEY } from '../constants/auth';
 import { UserRepository } from '../repository/user.repository';
 import { UserService } from '../service/user.service';
-import { BusinessLogic } from "../shared/BusinessLogicInterface";
+import { BusinessLogic } from '../shared/BusinessLogicInterface';
 import {
-    ShowUserInfoResObj,
-    UserInfo,
-    UserInfoResObj,
-    UserLoginInfo,
-    UserTokenResObj,
-    UserUpdateInfo,
+	ShowUserInfoResObj,
+	UserInfo,
+	UserInfoResObj,
+	UserLoginInfo,
+	UserTokenResObj,
+	UserUpdateInfo,
 } from '../shared/DataTransferObject';
 
 export class UserController {
-    private userService: UserService = new UserService(UserRepository.getQueryRepository());
+	private userService: UserService = new UserService(UserRepository.getQueryRepository());
 
-    public createUser: BusinessLogic = async (req, res, next) => {
-        const userInfoToCreate = req.body as UserInfo;
+	public createUser: BusinessLogic = async (req, res, next) => {
+		const userInfoToCreate = req.body as UserInfo;
 
-        const response: UserTokenResObj = await this.userService.createUser(userInfoToCreate);
-        return res.status(201).json(response);
-    };
+		const response: UserTokenResObj = await this.userService.createUser(userInfoToCreate);
+		return res.status(201).json(response);
+	};
 
-    public login: BusinessLogic = async (req, res, next) => {
-        const userInfoToLogin = req.body as UserLoginInfo;
+	public login: BusinessLogic = async (req, res, next) => {
+		const userInfoToLogin = req.body as UserLoginInfo;
 
-        const response: UserTokenResObj = await this.userService.login(userInfoToLogin);
-        return res.status(200).json(response);
-    }
+		const response: UserTokenResObj = await this.userService.login(userInfoToLogin);
+		return res.status(200).json(response);
+	};
 
-    public updateInfo: BusinessLogic = async (req, res, next) => {
-        const { nickname } = req.body;
-        const { id } = req.decoded;
+	public updateInfo: BusinessLogic = async (req, res, next) => {
+		const { nickname } = req.body;
+		const { id } = req.decoded;
 
-        const response: UserUpdateInfo = await this.userService.updateUserInfo({ nickname, id });
-        return res.status(200).json(response);
-    };
+		const response: UserUpdateInfo = await this.userService.updateUserInfo({ nickname, id });
+		return res.status(200).json(response);
+	};
 
-    public cancelMember: BusinessLogic = async (req, res, next) => {
-        const response = await this.userService.cancelMember(req.decoded.id, req.body.password);
-    
-        return res.status(204).json(response);
-      };
+	public logout: BusinessLogic = async (req, res, next) => {
+		try {
+			res.clearCookie(REFRESH_TOKEN_COOKIE_KEY);
+			return res.status(204).end();
+		} catch (err) {
+			next(err);
+		}
+	};
 
-    public refreshToken: BusinessLogic = async (req, res, next) => {
-        const refreshToken: string = req.headers.authorization['refresh-token'] as string;
-        const response: UserTokenResObj = await this.userService.refreshToken(
-            req.decoded.sub,
-            refreshToken.slice(7),
-        );
-        return res.status(200).json(response);
-    };
+	public cancelMember: BusinessLogic = async (req, res, next) => {
+		const response = await this.userService.cancelMember(req.decoded.id, req.body.password);
 
-    public showUserInfo: BusinessLogic = async (req, res, next) => {
-        const response: UserInfoResObj = await this.userService.showUserInfo(req.params.id);
-        return res.status(200).json(response);
-    };
+		return res.status(204).json(response);
+	};
 
-    public showMyInfo: BusinessLogic = async (req, res, next) => {
+	public refreshToken: BusinessLogic = async (req, res, next) => {
+		const refreshToken: string = req.headers.authorization['refresh-token'] as string;
+		const response: UserTokenResObj = await this.userService.refreshToken(
+			req.decoded.sub,
+			refreshToken.slice(7),
+		);
+		return res.status(200).json(response);
+	};
+
+	public showUserInfo: BusinessLogic = async (req, res, next) => {
+		const response: UserInfoResObj = await this.userService.showUserInfo(req.params.id);
+		return res.status(200).json(response);
+	};
+
+	public showMyInfo: BusinessLogic = async (req, res, next) => {
 		const user = req.decoded;
 
 		const response: ShowUserInfoResObj = await this.userService.showMyInfo(user);
 		return res.status(200).json(response);
 	};
-
-    public logout: BusinessLogic = async (req, res, next) => {
-        try {
-            res.clearCookie(REFRESH_TOKEN_COOKIE_KEY);
-            return res.status(204).end();
-        } catch (err) {
-            next(err);
-        }
-    };
 }
