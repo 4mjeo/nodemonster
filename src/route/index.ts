f61b6914bccd59904d6e25538cfe19edcc73f62d
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-08 13:23:36 +0900

    update: add restaurantRouter to index.ts

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { userServiceRouter } from './user.router';
+import { restaurantServiceRouter } from './restaurant.router';
 
 export const nodemonsterRouter = () => {
     const app = Router();
 
     userServiceRouter(app);
+    restaurantServiceRouter(app);
 
     return app;
 };
