16611c7c79343b3758c4f8cdb2b2816a568d8696
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-10 21:09:42 +0900

    update: add validate methods

--- src/service/menu.service.ts
+++ src/service/menu.service.ts
@@ -1,10 +1,10 @@
 import { MenuRepository } from '../repository/menu.repository';
-import { RestaurantRepository } from '../repository/restaurant.repository';
+import { RestaurantService } from './restaurant.service';
 import { Menu } from '../entity/menu';
 import { Restaurant } from '../entity/restaurant';
 import { NotFoundError, ForbiddenError } from '../shared/exception';
 import { MenuInfo } from '../shared/DataTransferObject';
-import { RestaurantService } from './restaurant.service';
+import { User } from '../entity/user';
 
 export class MenuService {
     constructor(
@@ -12,54 +12,64 @@ export class MenuService {
         private restaurantService: RestaurantService
     ) {}
 
-    async createMenu(menuInfo: MenuInfo): Promise<Menu> {
+    async createMenu(menuInfo: MenuInfo, user: User): Promise<Menu> {
         const restaurant = await this.restaurantService.getOneRestaurant(menuInfo.restaurantId);
         if (!restaurant) {
             throw new NotFoundError('Restaurant not found');
         }
 
-        return this.menuRepository.createMenu(menuInfo, restaurant)
+        await this.checkIsOwner(restaurant, user);
+
+        return this.menuRepository.createMenu(menuInfo, restaurant);
     }
 
-    async updateMenu(menuInfo: MenuInfo): Promise<void> {
+    async updateMenu(menuInfo: MenuInfo, user: User): Promise<void> {
         const menu = await this.menuRepository.findOne(menuInfo.id);
-        if (!menu) {
-            throw new NotFoundError('Menu not found');
-        }
+        await this.checkMenuExist(menu);
 
-        return this.menuRepository.updateMenu(menuInfo);
+        const restaurant = await this.restaurantService.getOneRestaurant(menuInfo.restaurantId);
+        if (!restaurant) {
+            throw new NotFoundError('Restaurant not found');
+        }
+        await this.checkIsOwner(restaurant, user);
+
+        await this.menuRepository.updateMenu(menuInfo);
     }
 
     async getMenuById(menuId: number): Promise<Menu> {
         const menu = await this.menuRepository.findOne(menuId);
-        if (!menu) {
-            throw new NotFoundError('Menu not found');
-        }
-
+        await this.checkMenuExist(menu);
         return menu;
     }
 
-    async getMenuByRestaurant(restaurantId: number): Promise<Menu[]> {
-        const restaurant = await this.restaurantService.getOneRestaurant(restaurantId);
+    async getMenusByRestaurant(restaurantId: number): Promise<Menu[]> {
+        await this.restaurantService.getOneRestaurant(restaurantId);
+
+        return this.menuRepository.getMenuByRestaurant(restaurantId);
+    }
+
+    async deleteMenu(menuId: number, user: User): Promise<void> {
+        const menu = await this.menuRepository.findOne(menuId);
+        await this.checkMenuExist(menu);
+
+        const restaurant = await this.restaurantService.getOneRestaurant(menu.restaurant.id);
         if (!restaurant) {
             throw new NotFoundError('Restaurant not found');
         }
-
-        return this.menuRepository.getMenuByRestaurant(restaurantId);
-    } 
-
-    async deleteMenu(menuId: number): Promise<void> {
-        const menu = await this.menuRepository.fidnOne(menuId);
-        if (!menu) {
-            throw new NotFoundError('Menu not found');
-        }
+        await this.checkIsOwner(restaurant, user);
 
         await this.menuRepository.deleteMenu(menuId);
     }
 
-    async checkMenuExist(menu: Menu) {
-        if(!menu) {
+    private async checkMenuExist(menu: Menu) {
+        if (!menu) {
             throw new NotFoundError('Menu not found');
+        }
+    }
+
+    private async checkIsOwner(restaurant: Restaurant, user: User) {
+        if (restaurant.user.id !== user.id) {
+            throw new ForbiddenError('You can only update menus for your own restaurant');
         }
     }
 }
