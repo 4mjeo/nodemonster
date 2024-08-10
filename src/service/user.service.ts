66f36d2b5f40e911abb59d193ce3b9d058f0a392
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-10 19:26:59 +0900

    update: add userType when login

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import { config } from '../config';
 import { User } from '../entity/user';
 import { UserRepository } from '../repository/user.repository';
+import { UserType } from '../entity/enum/usertype';
 import {
 	UpdateInfo,
 	UserInfo,
@@ -35,18 +36,18 @@ export class UserService {
 		if (alreadyRegisteredUser) {
 			throw new ConflictError();
 		}
-
+	
 		const hashedPassword = generateHash(userInfo.password);
-
+	
 		const userInfoToCreate = { ...userInfo, password: hashedPassword };
-		const { id, createdAt, updatedAt } = await this.userRepository.createUser(userInfoToCreate);
-
+		const { id, createdAt, updatedAt, type } = await this.userRepository.createUser(userInfoToCreate);
+	
 		return {
 			id,
 			createdAt,
 			updatedAt,
-			access_token: await this.issuanceToken(userInfo.email, 'access'),
-			refresh_token: await this.issuanceToken(userInfo.email, 'refresh'),
+			access_token: await this.issuanceToken(userInfo.email, type, 'access'),
+			refresh_token: await this.issuanceToken(userInfo.email, type, 'refresh'),
 		};
 	}
 
@@ -63,15 +64,14 @@ export class UserService {
 		}
 
 		return {
-			access_token: await this.issuanceToken(user.email, 'access'),
-			refresh_token: await this.issuanceToken(user.email, 'refresh'),
+			access_token: await this.issuanceToken(user.email, user.type, 'access'),
+			refresh_token: await this.issuanceToken(user.email, user.type, 'refresh'),
 		};
 	}
 
 	async updateUserInfo(userUpdateInfo: UserUpdateInfo): Promise<UserUpdateInfo & UpdateInfo> {
 		const user = await this.userRepository.findUserById(userUpdateInfo.id);
 
-		console.log(user);
 		if (!user) {
 			throw new ForbiddenError();
 		} else {
@@ -91,7 +91,7 @@ export class UserService {
 		const user = await this.userRepository.findUserById(id);
 
 		if (!user) throw new NotFoundError('User Not Found');
-		console.log(password, user, id);
+
 		const isValid = comparePassword(user.password, password);
 		if (!isValid) {
 			throw new UnAuthorizedError('Password MisMatch Error');
@@ -101,7 +101,11 @@ export class UserService {
 	}
 
 	public async refreshToken(email: string, refreshToken: string): Promise<UserTokenResObj> {
-		const accessToken: string = await this.issuanceToken(email, 'access');
+		const user = await this.userRepository.findUserByEmail(email);
+		if (!user) {
+			throw new NotFoundError('User Not Found');
+		}
+		const accessToken: string = await this.issuanceToken(email, user.type, 'access');
 		return {
 			access_token: accessToken,
 			refresh_token: refreshToken,
@@ -121,12 +125,13 @@ export class UserService {
 		return this.userRepository.getMyInfo(user);
 	}
 
-	private async issuanceToken(email: string, type: string): Promise<string> {
-		const user = this.userRepository.findUserByEmail(email);
+	private async issuanceToken(email: string, userType: UserType, type: string): Promise<string> {
+		const user = await this.userRepository.findUserByEmail(email);
 		return jwt.sign(
 			{
 				sub: `${email}`,
-				id: (await user).id,
+				id: user.id,
+				userType,
 				type,
 			},
 			config.jwtSecret,
