b420d8d2ed8dd3b2876627b7e5df2164795b1e8c
4mjeo <4mj.eo@dsm.hs.kr>
2024-08-13 23:37:29 +0900

    update: add menuRouterService to index

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { userServiceRouter } from './user.router';
 import { restaurantServiceRouter } from './restaurant.router';
+import { menuServiceRouter } from './menu.router';
 
 export const nodemonsterRouter = () => {
     const app = Router();
 
     userServiceRouter(app);
     restaurantServiceRouter(app);
+    menuServiceRouter(app);
 
     return app;
 };
